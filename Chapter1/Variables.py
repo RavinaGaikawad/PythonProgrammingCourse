37f1a012479ba41d4465714972d68ed473426fd8
Ravina Gaikawad <ravina.gaikawad@edf-re.com>
2024-01-08 10:19:57 -0600

    Chapter 2

---
+++ Chapter1/Variables.py
@@ -0,0 +1,147 @@
+# Variables are objects. They are used to store values.
+
+# Define variables. variable_name = value
+age = 21
+
+# Variables are case-sensitive
+age = 21
+Age = 22
+
+# print(Age)
+
+
+
+
+
+
+
+
+# Variable Types
+
+# Integer
+players = 11
+seats = 5
+print('Number of seats = ', seats)
+
+
+play = 123
+print(play)
+
+
+
+
+
+
+# Float
+distance = 1.5
+gpa = 3.5
+
+print('My gpa is', gpa)
+
+
+
+
+
+
+
+
+
+# String
+name = 'Pseudocoder Ravina'
+email = 'ravina123@gmail.com'
+
+print(email)
+
+
+
+
+
+
+
+
+
+
+# Boolean
+is_running = True
+is_cold = True
+
+if is_cold:
+    print("Chicago is cold")
+else:
+    print("Chicago is not cold")
+
+
+
+
+
+
+
+
+
+
+# Variables reusability
+age = 21
+age = 'you are old'
+print(age)
+print(type(age))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Casting
+age = 21
+float_age = float(age)
+
+print(float_age)
+print(type(float_age))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+''' Take Home Question
+You have 2 variables
+var1 = 'Welcome to '
+var2 = 2024
+Create a third variable using these 2 variables such that
+the final output is 'Welcome to 2024'
+'''
